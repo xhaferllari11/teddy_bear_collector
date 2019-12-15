d22065533995ffc664a617b5cff61ffc59a506d2
Alban <alban.xhaferllari@gmail.com>
2019-12-15 15:57:34 -0600

    created pages and links

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'teddys/index.html')
+
+
