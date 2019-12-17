15d9b31884234e8d80783348f554854c85fb9fd1
Alban <alban.xhaferllari@gmail.com>
2019-12-16 20:43:35 -0600

    created cleaning model, saving before migrations

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+DIRTINESS = (
+    ('D', 'Diry'),
+    ('S', 'Somewhat Dirty'),
+    ('C', 'Clean')
+)
+
 class Teddy(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -14,3 +20,18 @@ class Teddy(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'teddy_id': self.id})
+
+class Cleaning(models.Model):
+    date = models.DateField()
+    dirt = models.CharField(
+        max_length=1,
+        choices=DIRTINESS,
+        default=DIRTINESS[0][0]
+    )
+    teddy = models.ForeignKey(Teddy, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_dirt_display()} on {self.date}"
+
+
+        
